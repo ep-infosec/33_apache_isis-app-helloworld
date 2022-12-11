3ed2bbdc7225c9698592247916c8ac6bdfe190a8
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-11 22:47:48 +0200

    init

---
+++ src/main/java/domainapp/modules/hello/dom/hwo/HelloWorldObject.java
@@ -0,0 +1,146 @@
+package domainapp.modules.hello.dom.hwo;
+
+import java.util.Comparator;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Version;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.PropertyLayout;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.annotation.Title;
+import org.apache.isis.applib.layout.LayoutConstants;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+import org.apache.isis.persistence.jpa.applib.integration.IsisEntityListener;
+
+import domainapp.modules.hello.types.Name;
+import domainapp.modules.hello.types.Notes;
+
+@Entity
+@Table(
+        schema="hello",
+        name = "HelloWorldObject",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "HelloWorldObject__name__UNQ", columnNames = {"name"})
+        }
+)
+@EntityListeners(IsisEntityListener.class) // injection support
+@Named("hello.HelloWorldObject")
+@DomainObject()
+@DomainObjectLayout()  // causes UI events to be triggered
+public class HelloWorldObject implements Comparable<HelloWorldObject> {
+
+    protected HelloWorldObject(){}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, name = "id")
+    private Long id;
+
+    @Version
+    @Column(nullable = false, name = "version")
+    private int version;
+
+
+    public HelloWorldObject(final String name) {
+        this.name = name;
+    }
+
+
+
+    @Column(length = Name.MAX_LEN, nullable = false, name = "name")
+    private String name;
+
+    @Title(prepend = "Object: ")
+    @Name
+    @PropertyLayout(fieldSetId = LayoutConstants.FieldSetId.IDENTITY, sequence = "1")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    @Column(length = Notes.MAX_LEN, nullable = true, name = "notes")
+    private String notes;
+
+    @Notes
+    @PropertyLayout(fieldSetId = LayoutConstants.FieldSetId.DETAILS, sequence = "1")
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+
+
+    @Action(
+            semantics = SemanticsOf.IDEMPOTENT,
+            executionPublishing = Publishing.ENABLED
+    )
+    @ActionLayout(
+            associateWith = "name",
+            describedAs = "Updates the object's name"
+    )
+    public HelloWorldObject updateName(
+            @Name final String name) {
+        setName(name);
+        return this;
+    }
+    public String default0UpdateName() {
+        return getName();
+    }
+
+
+
+    @Action(
+            semantics = SemanticsOf.NON_IDEMPOTENT_ARE_YOU_SURE
+    )
+    @ActionLayout(
+            fieldSetId = LayoutConstants.FieldSetId.IDENTITY,
+            describedAs = "Deletes this object from the database",
+            position = ActionLayout.Position.PANEL
+    )
+    public void delete() {
+        final String title = titleService.titleOf(this);
+        messageService.informUser(String.format("'%s' deleted", title));
+        repositoryService.removeAndFlush(this);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public int compareTo(final HelloWorldObject other) {
+        return Comparator.comparing(HelloWorldObject::getName).compare(this, other);
+    }
+
+
+    @Inject @Transient RepositoryService repositoryService;
+    @Inject @Transient TitleService titleService;
+    @Inject @Transient MessageService messageService;
+
+}
---
+++ src/main/java/domainapp/modules/hello/dom/hwo/HelloWorldObjects.java
@@ -0,0 +1,61 @@
+package domainapp.modules.hello.dom.hwo;
+
+import java.util.List;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.PriorityPrecedence;
+import org.apache.isis.applib.annotation.PromptStyle;
+import org.apache.isis.applib.annotation.RestrictTo;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.repository.RepositoryService;
+
+import domainapp.modules.hello.types.Name;
+
+@Named("hello.HelloWorldObjects")
+@DomainService(nature = NatureOfService.VIEW)
+@Priority(PriorityPrecedence.EARLY)
+public class HelloWorldObjects {
+
+    private final RepositoryService repositoryService;
+    private final HelloWorldRepository helloWorldRepository;
+
+    @Inject
+    public HelloWorldObjects(
+            final RepositoryService repositoryService,
+            final HelloWorldRepository helloWorldRepository) {
+        this.repositoryService = repositoryService;
+        this.helloWorldRepository = helloWorldRepository;
+    }
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_MODAL)
+    public HelloWorldObject create(
+            @Name final String name) {
+        return repositoryService.persist(new HelloWorldObject(name));
+    }
+    public String default0Create() {
+        return "Hello World!";
+    }
+
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public List<HelloWorldObject> findByName(
+            @Name final String name) {
+        return helloWorldRepository.findByNameContaining(name);
+    }
+
+    @Action(semantics = SemanticsOf.SAFE, restrictTo = RestrictTo.PROTOTYPING)
+    public List<HelloWorldObject> listAll() {
+        return helloWorldRepository.findAll();
+    }
+
+
+}
